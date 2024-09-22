89991e19bb8f29f205c267e2425ad7a447d6fbdd
Om Satapathy <omsatapathy05@gmail.com>
2024-09-22 19:33:21 +0530

    project init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Hero from "./sections/Hero.tsx";
+import Navbar from "./sections/Navbar.tsx";
+function App() {
+
+  return (
+    <main className="max-w-7xl mx-auto">
+      <Navbar />
+      <Hero />
+    </main>
+  )
+}
+
+export default App
---
+++ src/sections/Hero.tsx
@@ -0,0 +1,47 @@
+import { PerspectiveCamera } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
+import HackerRoom from "../components/HackerRoom"
+import { Suspense } from "react"
+import CanvasLoader from "../components/CanvasLoader"
+import { useMediaQuery } from "react-responsive"
+import { calculateSizes } from "../constants"
+
+function Hero() {
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024, });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I am Om Satapathy
+          <span className="waving-hand pl-3 ">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">Building things that I enjoy</p>
+      </div>
+
+
+      <div className="w-full h-full absolute inset-0">
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+            <HackerRoom
+              position={sizes.deskPosition}
+              rotation={[0, -Math.PI, 0]}
+              scale={sizes.deskScale}
+
+            />
+
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
